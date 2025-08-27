606fffdc2dfbed9e5e1e05269b77d8ba5c602f12
kiettt23 <kiettt23@gmail.com>
2025-08-27 11:21:53 +0700

    feat(timer): add split/lap state and logic

--- src/useTimer.js
+++ src/useTimer.js
@@ -2,8 +2,8 @@ import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
   const [time, setTime] = useState(ini);
+  const [laps, setLaps] = useState([]);
 
-  const isStart = "Your code here";
   const active = useRef(null);
   const refInterval = useRef(null);
 
@@ -12,13 +12,11 @@ const useTimer = (ini = 0) => {
       refInterval.current = setInterval(() => {
         setTime((prev) => prev + 1);
       }, 1000);
-      console.log(refInterval.current);
       active.current.disabled = true;
     }
   };
   const stopTimer = () => {
     clearInterval(refInterval.current);
-    console.log(refInterval.current);
     refInterval.current = null;
     if (active.current) active.current.disabled = false;
   };
@@ -26,10 +24,14 @@ const useTimer = (ini = 0) => {
     clearInterval(refInterval.current);
     refInterval.current = null;
     setTime(0);
+    setLaps([]);
     if (active.current) active.current.disabled = false;
   };
+  const splitTimer = () => {
+    setLaps((prev) => [...prev, time]);
+  };
 
-  return { time, startTimer, active, stopTimer, resetTimer };
+  return { time, laps, startTimer, active, stopTimer, resetTimer, splitTimer };
 };
 
 export default useTimer;
